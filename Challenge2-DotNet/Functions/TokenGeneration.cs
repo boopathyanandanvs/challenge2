ce65f5f04625960e0439ba4c9be5f21255e7496c
boopathyanandanvs <sboopathyanandan@gmail.com>
2024-05-23 01:16:28 +0530

    init

---
+++ Challenge2-DotNet/Functions/TokenGeneration.cs
@@ -0,0 +1,49 @@
+﻿using Challenge2_DotNet.Model;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System.Net;
+
+namespace Challenge2_DotNet.Functions
+{
+    public interface ITokenGeneration
+    {
+        Task<string> generateTokenAsync(Request metaDataRequest);
+    }
+    public class TokenGeneration: ITokenGeneration
+    {
+        private readonly HttpClient _httpClient;
+
+        public TokenGeneration(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+
+        }
+
+        public async Task<string> generateTokenAsync(Request request)
+        {
+            string token = string.Empty;
+
+            var requestUrl = "https://login.microsoftonline.com/" + request.TenantId + "/oauth2/token";
+
+            var values = new List<KeyValuePair<string, string>>();
+            values.Add(new KeyValuePair<string, string>("client_id", request.ClientId));
+            values.Add(new KeyValuePair<string, string>("client_secret", request.ClientSecret));
+            values.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
+            values.Add(new KeyValuePair<string, string>("resource", "https://management.azure.com"));
+            values.Add(new KeyValuePair<string, string>("cache-control", "no-cache"));
+
+            var requestBody = new FormUrlEncodedContent(values);
+            var response = await _httpClient.PostAsync(requestUrl, requestBody);
+            if (response.IsSuccessStatusCode)
+            {
+                string responseStr = await response.Content.ReadAsStringAsync();
+                var content = JsonConvert.DeserializeObject<AzureADTokenResponse>(responseStr);
+                return content.access_token;
+            }
+            else
+            {
+                throw new Exception($"Failed to generate token - {response.StatusCode} - {response.ReasonPhrase}");   
+            }
+        }
+    }
+}
---
+++ Challenge2-DotNet/Model/Request.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Challenge2_DotNet.Model
+{
+    public class Request
+    {
+        [Required]
+        public string ClientId { get; set; }
+        [Required]
+        public string ClientSecret { get; set; }
+        [Required]
+        public string TenantId { get; set; }
+        [Required]
+        public string SubscriptionId { get; set; }
+        [Required]
+        public string ResourceGroup { get; set; }
+        [Required]
+        public string ResourceProvider { get; set; }
+        [Required]
+        public string ApiVersion { get; set; }
+        [Required]
+        public string ResourceType { get; set; }
+        [Required]
+        public string ResourceName { get; set; }
+    }
+}
